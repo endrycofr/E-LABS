89defef5efcfde9a6e6b7b27bec3b091aec84b42
endrycofr <endrycofarelr@gmail.com>
2023-06-07 00:08:02 +0700

    menambahkan fitur

---
+++ app/src/main/java/com/zakiabdullah/medical/HomeActivity.java
@@ -0,0 +1,43 @@
+package com.zakiabdullah.medical;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        SharedPreferences sharedpreferences = getSharedPreferences ("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedpreferences.getString("username", "").toString();
+        Toast.makeText(getApplicationContext(),"Welcome"+username,Toast.LENGTH_SHORT).show();
+
+        CardView exit = findViewById(R.id.cardExit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(HomeActivity.this,loginActivity.class));
+            }
+        });
+
+        CardView fasilitas = findViewById(R.id.cardFasilitas);
+        fasilitas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,introActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/zakiabdullah/medical/loginActivity.java
@@ -0,0 +1,66 @@
+package com.zakiabdullah.medical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class loginActivity extends AppCompatActivity {
+
+    EditText edUsername, edPassword;
+    Button btn;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        edUsername = findViewById(R.id.editTextLoginUsername);
+        edPassword = findViewById(R.id.editTextLoginPassword);
+        btn = findViewById(R.id.buttonLogin);
+        tv = findViewById(R.id.textViewNewUser);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                Database db = new Database(getApplicationContext(),"healthcare",null,1);
+                if(username.length()==0 || password.length()==0){
+                    Toast.makeText(getApplicationContext(),"Please fill All details",Toast.LENGTH_SHORT).show();
+                    }else{
+                    if(db.login(username,password)==1){
+                        Toast.makeText(getApplicationContext(),"Login Success",Toast.LENGTH_SHORT).show();
+                        SharedPreferences sharedpreferences = getSharedPreferences ("shared_prefs", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor =sharedpreferences.edit();
+                        editor.putString("username", username);
+//                      //to save our data with key and value.
+                        editor.apply();
+//                        startActivity(new Intent(loginActivity.this,HomeActivity.class));
+                        startActivity(new Intent(loginActivity.this,tentangActivity.class));
+                    }else{
+                        Toast.makeText(getApplicationContext(),"Invalid Username and Password",Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+            }
+        });
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(loginActivity.this,RegisterActivity.class));
+            }
+        });
+
+    }
+}
